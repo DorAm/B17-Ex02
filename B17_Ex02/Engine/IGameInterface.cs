2322c4499bbbbae241aa9a0cf6e3af3243d08064
DorAmar <bndor.am@gmail.com>
2017-05-27 15:44:39 +0300

    added numOfRounds getter to the interface

--- B17_Ex02/Engine/IGameInterface.cs
+++ B17_Ex02/Engine/IGameInterface.cs
@@ -9,6 +9,7 @@ namespace B17_Ex02
         void StartNewGame();
         void makeGuess(Guess i_UserGuess);
 
+        bool NumOfRounds { get; }
         bool IsGameOver { get; }
         bool IsVictory { get; }        
         List<Guess> GuessList { get; }
