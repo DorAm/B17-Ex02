9d81153cdd166883844ad773aa9ed71c566a071f
DorAmar <bndor.am@gmail.com>
2017-05-27 13:40:57 +0300

    added access modifiers

--- B17_Ex02/Engine/GuessResult.cs
+++ B17_Ex02/Engine/GuessResult.cs
@@ -3,9 +3,9 @@
     public class GuessResult
     {
         // a symbol that appears in the same sequence location
-        int m_BulHits = 0;
+        private int m_BulHits = 0;
         // a symbol that appears in the sequence but not in the same location
-        int m_PgiyaHits = 0;
+        private int m_PgiyaHits = 0;
 
         public int BulHits { get => m_BulHits; set => m_BulHits = value; }
         public int PgiyaHits { get => m_PgiyaHits; set => m_PgiyaHits = value; }
